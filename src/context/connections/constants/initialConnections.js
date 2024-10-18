97629c92a417c0f763c5e4551b4818d6b96609fa
chamce <chancellor.whitaker@eku.edu>
2024-10-18 14:36:20 -0400

    build w constant connections

--- src/context/connections/constants/initialConnections.js
+++ src/context/connections/constants/initialConnections.js
@@ -1,3 +1,238 @@
 import { initializeConnections } from "../helpers/initializeConnections";
 
-export const initialConnections = initializeConnections(25);
+const constant = [
+  [
+    {
+      id: "Schoen LLC",
+      list: "groups",
+    },
+    {
+      id: "https://direct-tribe.com/",
+      list: "reports",
+    },
+  ],
+  [
+    {
+      id: "Bechtelar LLC",
+      list: "groups",
+    },
+    {
+      id: "https://bitter-coin.biz",
+      list: "reports",
+    },
+  ],
+  [
+    {
+      id: "Quigley LLC",
+      list: "groups",
+    },
+    {
+      id: "Javon.Strosin45@hotmail.com",
+      list: "users",
+    },
+  ],
+  [
+    {
+      id: "Javon.Strosin45@hotmail.com",
+      list: "users",
+    },
+    {
+      id: "Bechtelar LLC",
+      list: "groups",
+    },
+  ],
+  [
+    {
+      id: "Trantow LLC",
+      list: "groups",
+    },
+    {
+      id: "https://pleasing-invite.net/",
+      list: "reports",
+    },
+  ],
+  [
+    {
+      id: "https://wonderful-transom.net",
+      list: "reports",
+    },
+    {
+      id: "Javon.Strosin45@hotmail.com",
+      list: "users",
+    },
+  ],
+  [
+    {
+      id: "https://soggy-scholarship.net",
+      list: "reports",
+    },
+    {
+      id: "Dianna_Hessel-Green52@gmail.com",
+      list: "users",
+    },
+  ],
+  [
+    {
+      id: "Trantow LLC",
+      list: "groups",
+    },
+    {
+      id: "Javon.Strosin45@hotmail.com",
+      list: "users",
+    },
+  ],
+  [
+    {
+      id: "Edythe.Adams59@hotmail.com",
+      list: "users",
+    },
+    {
+      id: "https://urban-igloo.name",
+      list: "reports",
+    },
+  ],
+  [
+    {
+      id: "https://pure-duster.info",
+      list: "reports",
+    },
+    {
+      id: "Stiedemann - Sawayn",
+      list: "groups",
+    },
+  ],
+  [
+    {
+      id: "https://pleasing-invite.net/",
+      list: "reports",
+    },
+    {
+      id: "Allen.Zulauf@hotmail.com",
+      list: "users",
+    },
+  ],
+  [
+    {
+      id: "Bednar, Zieme and O'Connell",
+      list: "groups",
+    },
+    {
+      id: "https://wonderful-transom.net",
+      list: "reports",
+    },
+  ],
+  [
+    {
+      id: "Schaefer, Hagenes and Kreiger",
+      list: "groups",
+    },
+    {
+      id: "https://perfumed-seal.biz",
+      list: "reports",
+    },
+  ],
+  [
+    {
+      id: "Schoen LLC",
+      list: "groups",
+    },
+    {
+      id: "https://agile-kit.com/",
+      list: "reports",
+    },
+  ],
+  [
+    {
+      id: "https://perfumed-seal.biz",
+      list: "reports",
+    },
+    {
+      id: "Dianna_Hessel-Green52@gmail.com",
+      list: "users",
+    },
+  ],
+  [
+    {
+      id: "Allen.Zulauf@hotmail.com",
+      list: "users",
+    },
+    {
+      id: "https://direct-tribe.com/",
+      list: "reports",
+    },
+  ],
+  [
+    {
+      id: "https://soggy-scholarship.net",
+      list: "reports",
+    },
+    {
+      id: "Bechtelar LLC",
+      list: "groups",
+    },
+  ],
+  [
+    {
+      id: "https://agile-kit.com/",
+      list: "reports",
+    },
+    {
+      id: "Allen.Zulauf@hotmail.com",
+      list: "users",
+    },
+  ],
+  [
+    {
+      id: "Schaefer, Hagenes and Kreiger",
+      list: "groups",
+    },
+    {
+      id: "Horace72@gmail.com",
+      list: "users",
+    },
+  ],
+  [
+    {
+      id: "Madalyn_Lemke@hotmail.com",
+      list: "users",
+    },
+    {
+      id: "https://urban-igloo.name",
+      list: "reports",
+    },
+  ],
+  [
+    {
+      id: "https://agile-kit.com/",
+      list: "reports",
+    },
+    {
+      id: "Braun, Ebert and Windler",
+      list: "groups",
+    },
+  ],
+  [
+    {
+      id: "Berge - Smith",
+      list: "groups",
+    },
+    {
+      id: "Dianna_Hessel-Green52@gmail.com",
+      list: "users",
+    },
+  ],
+  [
+    {
+      id: "Allen.Zulauf@hotmail.com",
+      list: "users",
+    },
+    {
+      id: "https://soggy-scholarship.net",
+      list: "reports",
+    },
+  ],
+];
+
+// export const initialConnections = initializeConnections(25);
+
+export const initialConnections = constant;
